08c8cc06437e198159496b411514b198a52b362e
Bradley Chery <Bradley434@hotmail.com>
2024-04-30 00:22:35 -0400

    updated files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { puppyList } from "./data.js"
 import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
@@ -12,19 +13,26 @@ function App() {
   return (
     <>
       <div className="App">
-        {featPupId && <p>{featPupId}</p>}
+
         {puppies.map((puppy) => {
-          return <p onClick={() => { setFeatPupId(puppy.id) }} key={puppy.id}>{puppy.name}</p>
+          return <p className="button" onClick={() => { setFeatPupId(puppy.id) }} key={puppy.id}>{puppy.name}</p>
         })}
+        {featPupId && <p>ID of Puppy: {featPupId}</p>}
         {featPupId && (
           <div>
             <h2>{featuredPup.name}</h2>
             <ul>
-              <li>Age: {featuredPup.age}</li>
-              <li>Email: {featuredPup.email}</li>
+              <li className="list">
+                <h4>Age: {featuredPup.age}</h4>
+              </li>
+              <li className="list">
+                <h4>Email: {featuredPup.email}</h4>
+              </li>
             </ul>
           </div>
         )}
+
+        <button className="btn btn-primary" onClick={() => { location.reload() }}>Reset Page</button>
       </div>
     </>
   )
