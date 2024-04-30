7283dfe437c3a8622ec80e7563b2af04f89d68f2
Bradley Chery <Bradley434@hotmail.com>
2024-04-29 21:18:19 -0400

    created a puppy map for the array

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
+import { puppyList } from "./data.js"
 
 function App() {
-
-
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log("PuppyList: ", puppies);
   return (
     <>
-      <div>
-
+      <div className="App">
+        {puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>
+        })}
       </div>
     </>
   )
